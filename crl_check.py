38614485474a7378844400230a990c35467c902d
franps <franps.94@hotmail.com>
2020-10-22 18:14:38 -0300

    refactored to give nagios friendly responses, and it can process both DER and PEM now

--- crl_check.py
+++ crl_check.py
@@ -1,25 +1,41 @@
 from datetime import datetime
 from OpenSSL import crypto
 import subprocess
+import sys
 
 today = datetime.now()
 
 def get_expiration_from_file(crlName):
-    nextUpdate = subprocess.check_output(["/usr/bin/openssl", "crl", "-inform", "PEM", "-noout", "-nextupdate", "-in", "testcrl.crl"], stderr=subprocess.STDOUT).decode('ascii')
-    nextUpdate = nextUpdate.split("=")[1][:-1]
+    with open(crlName) as crl:
+        try:
+            if "BEGIN X509 CRL" in crl.read():
+                format = "PEM"
+        except UnicodeDecodeError as e: # trying to read DER format will throw this error, not fancy but effective
+            format = "DER"
+
+    nextUpdate = subprocess.check_output(["/usr/bin/openssl", "crl", "-inform", format, "-noout", "-nextupdate", "-in", crlName], stderr=subprocess.STDOUT).decode('ascii')
+    nextUpdate = nextUpdate.split("=")[1][:-1] # Data before splitting: 'nextUpdate=Oct 28 04:57:01 2020 GMT\n'
     exp = datetime.strptime(nextUpdate,"%b %d %H:%M:%S %Y %Z")
 
     return exp
 
 def is_expired(exp):
-	days_to_exp = (exp-today).days
-	if days_to_exp <= 0 :
-		print ("Crl about to expire or already expired, run for your lives! : ",days_to_exp, "days to expiration")
-	if days_to_exp < 15 :
-		print ("Expiration in less than 15 days: ",days_to_exp, "days to expiration")
-	elif days_to_exp < 30: 
-		print ("Expiration in less than 30 days: ",days_to_exp, "days to expiration")
+    days_to_exp = (exp-today).days
+    if days_to_exp <= 3 :
+        print ("CRITICAL CRL about to expire or already expired, run for your lives! : ",days_to_exp, "days to expiration")
+        exitcode = 2
+    elif days_to_exp < 15 :
+        print ("CRITICAL CRL expiration in less than 15 days: ",days_to_exp, "days to expiration")
+        exitcode = 2 
+    elif days_to_exp < 30: 
+        print ("WARNING CRL expiration in less than 30 days: ",days_to_exp, "days to expiration")
+        exitcode = 1 
+    else:
+        print ("OK CRL expires in :",days_to_exp," days to expiration")
+        exitcode = 0
 
-exp = get_expiration_from_file('testcrl.crl')
-print("Crl expiraton: ", exp)
-is_expired(exp)
+    sys.exit(exitcode)
+
+#exp = get_expiration_from_file('testcrl.crl')
+#print("Crl expiraton: ", exp)
+#is_expired(exp)
